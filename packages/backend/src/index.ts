d0c8b33f3b70f4ab2b70f15f8850d7f1d21663ec
Danila Dergachev <dderg@protonmail.com>
2022-03-11 16:50:32 +0100

    database is working

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import dotenv from 'dotenv';
+dotenv.config({ path: `${__dirname}/../.env` });
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 const app = express();
 const port = 8080;
+
+
+// @TODO disconnect on shutdown or on error
+// await prisma.$disconnect()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,6 +23,11 @@ app.post('/register', (req, res) => {
   console.log(userId);
 
   res.json(JSON.stringify(req.body));
+});
+
+app.get('/test', async (req, res) => {
+  const posts = await prisma.post.findMany()
+  console.log(posts)
 });
 
 app.listen(port, () => {
