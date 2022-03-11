8427c4487d3a8aa8eb35cd534ecbbcc975d6fd87
Vlatko Trajkov <trajkovvlatko@gmail.com>
2022-03-11 14:57:42 +0100

    Add express and / route to backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hey from backend");
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (_, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Started at http://localhost:${port}`);
+});
