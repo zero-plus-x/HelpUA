eb201cfd58445816b16187dfd5d101a9c9c9e501
Vlatko Trajkov <trajkovvlatko@gmail.com>
2022-03-11 15:53:50 +0100

    Fix path for env file in bot

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -1,7 +1,7 @@
 import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: `${__dirname}/../.env` });
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
