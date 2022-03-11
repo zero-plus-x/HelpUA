8c0d0526ba9788017d0b8f8b3cb15133f3048f78
Vlatko Trajkov <trajkovvlatko@gmail.com>
2022-03-11 15:11:03 +0100

    Add telegraf and dotenv

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hey from bot");
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
