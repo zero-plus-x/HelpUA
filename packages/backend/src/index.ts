6abf734530aff463d347e1461a459559dec82e74
Vlatko Trajkov <trajkovvlatko@gmail.com>
2022-03-13 17:53:05 +0100

    Clean up backend index

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,38 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-// import dotenv from 'dotenv';
-// dotenv.config({ path: `${__dirname}/../.env` });
-
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
-const app = express();
 const port = 3000;
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/register', (req, res) => {
-  const { user_id: userId } = req.body;
-
-  console.log(userId);
-
-  res.json(JSON.stringify(req.body));
+  res.json(req.body);
 });
 
-app.get('/test', async (req, res) => {
+app.get('/test', async (_, res) => {
   const posts = await prisma.post.findMany();
-  console.log(posts);
+  res.json(posts);
 });
 
-app.get('/ping', (req, res) => {
-  console.log('got backend ping');
+app.get('/ping', (_, res) => {
   res.end('pong');
 });
 
 app.listen(port, () => {
-  console.log(`Started at http://localhost:${port}`);
+  console.log(`>> Backend ready at http://localhost:${port}`);
 });
 
 // @TODO disconnect on shutdown or on error
