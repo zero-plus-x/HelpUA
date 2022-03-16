3d49cf9864fe6ddff9ea92f699ff26f87fed6da3
alchi <alchi@alchis-MacBook-Pro.local>
2022-03-17 00:19:55 +0100

    add bot translations

--- packages/backend/src/translations/index.ts
+++ packages/backend/src/translations/index.ts
@@ -1,10 +1,40 @@
 import { UILanguage, Category } from '@prisma/client';
 import { Role } from '../types';
 
+export const BotOptionQuestion = {
+  [UILanguage.ENGLISH]: 'Please select an option',
+  [UILanguage.UKRAINIAN]: 'Виберіть варіант',
+  [UILanguage.RUSSIAN]: 'Пожалуйста, выберите опцию',
+}
+
+export const BotCategoryQuestion = {
+  [UILanguage.ENGLISH]: 'What category?',
+  [UILanguage.UKRAINIAN]: 'Яка категорія?',
+  [UILanguage.RUSSIAN]: 'Какая категория?',
+}
+
+export const BotOfferCreatedReply = {
+  [UILanguage.ENGLISH]: 'We will get back to you when we find who you can help',
+  [UILanguage.UKRAINIAN]: 'Ми повернемося до вас, коли знайдемо, кому ви можете допомогти',
+  [UILanguage.RUSSIAN]: 'Мы свяжемся с вами, когда найдем, кому вы можете помочь',
+}
+
+export const BotRequestCreatedReply = {
+  [UILanguage.ENGLISH]: 'We will try to find help as soon as possible',
+  [UILanguage.UKRAINIAN]: 'Ми постараємося знайти допомогу якомога швидше',
+  [UILanguage.RUSSIAN]: 'Мы постараемся найти помощь как можно скорее',
+}
+
+export const BotNoUserNameErrorReply = {
+  [UILanguage.ENGLISH]: 'In order to use this bot you need to have a username. You can add it in the settings.',
+  [UILanguage.UKRAINIAN]: 'Щоб користуватися цим ботом, потрібно мати ім’я користувача. Ви можете додати його в налаштуваннях.',
+  [UILanguage.RUSSIAN]: 'Чтобы использовать этого бота, у вас должно быть имя пользователя. Вы можете добавить его в настройках.',
+}
+
 export const UILanguageLabels = {
-    [UILanguage.ENGLISH]: 'English',
-    [UILanguage.UKRAINIAN]: 'Украї́нська мо́ва',
-    [UILanguage.RUSSIAN]: 'Русский язык',
+  [UILanguage.ENGLISH]: 'English',
+  [UILanguage.UKRAINIAN]: 'Украї́нська мо́ва',
+  [UILanguage.RUSSIAN]: 'Русский язык',
 }
 
 console.log(Role)
